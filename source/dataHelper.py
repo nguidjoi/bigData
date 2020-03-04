88d266c3a5ef796a89c78d85bfdfadda790fef50
nba <alain.nguidjoi.bell@gmail.com>
2020-03-04 20:54:03 +0100

    exploration

--- source/dataHelper.py
+++ source/dataHelper.py
@@ -4,11 +4,10 @@ import pandas as pd
 
 if __name__ == "__main__":
     def verify(dataSet):
-
-        print("\nFeatures : \n", dataSet.columns.tolist())
-        print("\nNull values :  ", dataSet.isnull().sum())
-        print("\nMissing values :  ", dataSet.isnull().sum().values.sum())
-        print("\nUnique values :  \n", dataSet.nunique())
+        print("\n Features : \n", dataSet.columns.tolist())
+        print("\n Null values :  ", dataSet.isnull().sum())
+        print("\n Missing values :  ", dataSet.isnull().sum().values.sum())
+        print("\n Unique values :  \n", dataSet.nunique())
 
     churnData = pd.read_csv(r"../data/dataset.csv")
     verify(churnData)
